9c769e4c69fbde2785f3f12d0c3ebd6134796539
zyzzyxis <13ezimm@gmail.com>
2021-03-04 20:13:05 -0700

    edit form exists but does not work

--- app/javascript/components/Book.js
+++ app/javascript/components/Book.js
@@ -3,8 +3,10 @@ import axios from 'axios'
 
 const Book = (props) => {
   const { title, author, id, deleteBook} = props
-  
-  
+  // const [showForm, setShowForm] = useState(false)
+  // const [title, setTitle] = useState('')
+  // const [author, setAuthor] = useState('')
+
   const deleteHandler = async (id) => {
     let res = await axios.delete(`/books/${id}`)
     deleteBook(res.data.id)
@@ -14,15 +16,26 @@ const Book = (props) => {
     console.log('this button is meant to edit')
   }
 
+  
+
   return (
     <div className="book-container">
       <h1>
-        {id}- {title}
+        {id} - {title}
+        </h1>
        <p> </p>
+       <p>Author: {author}</p>
        <button onClick={() => deleteHandler(id)}>Delete</button>
        <button onClick={() => editHandler(id)}>Edit</button>
-      </h1>
-      <p>Author: {author}</p>
+      {/* want form to show up under each book 
+      need it to link to the id to make sure to change the right thing */}
+      <form /*style={{display: "none"}} */onSubmit={editHandler}>
+        <p>Title</p>
+        <input value={title} onChange={(e) => setTitle(e.target.value)}/>
+        <p>Author</p>
+        <input value={author} onChange={(e) => setAuthor(e.target.value)}/>
+        <button type='submit'>Update</button>
+      </form>
     </div>
   )
 }
